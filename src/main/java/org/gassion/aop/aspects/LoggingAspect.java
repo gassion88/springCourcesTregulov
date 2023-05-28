79f66fdaf2c91c3db52c4e1799f1263364e2404f
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-28 16:31:40 +0300

    feat : logging

--- src/main/java/org/gassion/aop/aspects/LoggingAspect.java
+++ src/main/java/org/gassion/aop/aspects/LoggingAspect.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.gassion.aop.Book;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,25 @@ public class LoggingAspect {
         System.out.println("methodSignature.getMethod() : " + methodSignature.getMethod());
         System.out.println("methodSignature.getName() : " + methodSignature.getName());
         System.out.println("methodSignature.getReturnType() : " + methodSignature.getReturnType());
+
+        if (methodSignature.getName().equals("addBook")) {
+            Object[] args = joinPoint.getArgs();
+
+            for (Object obj : args) {
+                if (obj instanceof Book) {
+                    Book book = (Book) obj;
+
+                    System.out.println("Информация о книге : название " + book.getName() +
+                            "автор : " + book.getAuthor() +
+                            "год издания : " + book.getYearsOfPublication());
+                }
+                else if (obj instanceof String) {
+                    System.out.println("Книгу добавляет : " + obj);
+                }
+            }
+
+            System.out.println("------------------------------------------");
+        }
 
         System.out.println("beforeGetLoggingAdvice : log попытки получить книгу/журнал");
         System.out.println("------------------------------------------");
